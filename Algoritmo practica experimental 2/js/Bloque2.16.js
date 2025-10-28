b2e062365608c0a376917b5020485702c7d1cf52
Luis Bueno <luis123456bueno22@gmail.com>
2025-10-27 19:18:34 -0500

    Actualizar Bloque2.16.js

--- Algoritmo practica experimental 2/js/Bloque2.16.js
+++ Algoritmo practica experimental 2/js/Bloque2.16.js
@@ -1,18 +1,19 @@
 function Invertir_Numeros () {
-    let numeros=[], invertido=0, digito=0, número=0, i=0;
+    let numeros=[], invertido=0, digito=0, número=0, num=0, i=0;
     numeros=[123,456,789];
-    invertido=0
     número=0
     digito=0
     for (i=0;i<numeros.length;i++){
-        numero=numero[i];
+        numero=numero[i];
+        num=número
+        invertido=0
         while(numero>0){
             digito=numero%10;
             invertido=(invertido*10)+digito;
             numero=(numero-digito)/10
         }
-        console.log(`el numero invertido es ${invertido}`);
-        alert(`el numero invertido es ${invertido}`);
+        console.log(`el numero ${num} invertido es ${invertido}`);
+        alert(`el numero ${num} invertido es ${invertido}`);
      }
 }
 debugger
